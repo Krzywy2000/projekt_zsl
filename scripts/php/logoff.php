815b3b997090ef806334343b32a80ac1301e1a8e
Krzywy2000 <wiktorwiese2000@gmail.com>
2020-01-29 02:35:37 +0100

    delete mysqli_connect

--- scripts/php/logoff.php
+++ scripts/php/logoff.php
@@ -3,8 +3,7 @@
         require_once('db_connect.php');
 
         $connect = new mysqli($host, $db_user, $db_password, $db_name);
-        $ip = $_SERVER['REMOTE_ADDR'];   
-        mysqli_query("UPDATE users SET ip = '$ip' WHERE id = '$_SESSION[id]'");
+        $ip = $_SERVER['REMOTE_ADDR'];  
 
     session_unset();
         session_destroy();
