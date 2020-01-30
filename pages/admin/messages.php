dcc2683f72e8070eb663547cc8b7b48f61b4eb3b
Krzywy2000 <wiktorwiese2000@gmail.com>
2020-01-30 09:52:53 +0100

    30.01.2020

--- pages/admin/messages.php
+++ pages/admin/messages.php
@@ -1,37 +1,42 @@
 <main>
-    <div class="container">
-        <div class="col-md-6">
-            <form method="POST" action="../scripts/php/admin/messages.php">
-                <select>
-                    <option name=admin>
-                        Administrator
-                    </option>
-                    <option name=director>
-                        Dyrekcja
-                    </option>
-                    <option name=teacher>
-                        Nauczyciele
-                    </option>
-                </select>
-                    <?php
-                        require_once "./scripts/php/db_connect.php";
+    <div class="container"><br/><br/>
+        <H2 class="headline">Wiadomości odebrane</H2>
+        <div class="messages__bar">
+            <button>Nowa wiadomość</button>
+            <button>Kosz</button>
+            <button>Wysłane</button>
+        </div><br/>
+        
+        <?php
+            require_once("./scripts/php/db_connect.php");
+            $connect = new mysqli($host, $db_user, $db_password, $db_name);
 
-                        $connect = new mysqli($host, $db_user, $db_password, $db_name);
-                        $id = $_POST['admin'];
-                        if($id == true)
+            if ($result = @$connect->query("SELECT * FROM `messages`"))
+                {
+                    echo "<table>
+                    <tr class='main'>
+                        <td>Temat</td>
+                        <td>Nadawca</td>
+                        <td>Data</td>
+                        <td>Przeczytana</td>
+                        <td>Usuń</td>
+                    </tr>";
+                    $users = $result->num_rows;
+                    if($users>0)
+                    {
+                        while($row = $result->fetch_array())
                         {
-                            $result = @$connect->query("SELECT imie,nazwisko FROM users WHERE access = 'admin'");
-                            $users = $result->num_rows;
-                            if($users>0)
-                            {
-                                while($row = $result->fetch_array())
-                                {
-                                    echo "<input type='radio'>".$row['name']." ".$row['surname']."/>";
-                                }
-                            }
+                            echo "<tr>
+                                <td>".$row['subject']."</td>
+                                <td>".$row['from_mess']."</td>
+                                <td>".$row['data_mess']."</td>
+                                <td>".$row['readed']."</td>
+                            </tr>";
                         }
-                    ?>
-            </form>
-        </div>
+                    }
+                    echo "</table>";
+                }
+            $connect->close();
+?>
     </div>
 </main>
