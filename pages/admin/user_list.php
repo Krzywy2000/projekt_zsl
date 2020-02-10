77d741d5cbb06f992423e4bf67e646c96904d01e
Krzywy2000 <wiktorwiese2000@gmail.com>
2020-02-10 10:39:33 +0100

    10.02.2020 - modify user_list and styles

--- pages/admin/user_list.php
+++ pages/admin/user_list.php
@@ -5,12 +5,21 @@
     ?>
     <div class="container"><br/><br/>
         <H2 class="headline">Lista użytkowników</H2>
+        <div class="messages__bar__left">
+            <form method="POST" action="index_admin.php?page=user_list">
+                <input type="text" name="search" placeholder="Znajdź użytkownika"/>
+                <button type="submit">Znajdź</button>
+            </form>
+        </div>
         <div class="messages__bar">
-            <button class="button" onClick="">Dodaj nowego użytkownika</button>
-            <button class="button">Znajdź użytkownika</button>
+            <button class="button" onClick="new_user.php">Dodaj nowego użytkownika</button></br>
         </div><br/>
         <?php
-            if ($result = @$connect->query("SELECT * FROM `users`"))
+        
+            $search = @$_POST['search'];
+
+            if($search == EMPTY($search)) {
+                if ($result = @$connect->query("SELECT * FROM `users`"))
                 {
                     echo "<table>
                     <tr class='main'>
@@ -39,6 +48,37 @@
                     }
                     echo "</table>";
                 }
+            } else {
+                if ($result = @$connect->query("SELECT * FROM `users` where `name` like '%$search%' or `surname` like '%$search%'"))
+                {
+                    echo "<table>
+                    <tr class='main'>
+                        <td>Imie</td>
+                        <td>Nazwisko</td>
+                        <td>e-mail</td>
+                        <td>Ostatnie logowanie</td>
+                        <td>Ostatni adres IP</td>
+                        <td>Dostęp</td>
+                    </tr>";
+                    $users = $result->num_rows;
+                    if($users>0)
+                    {
+                        while($row = $result->fetch_array())
+                        {
+                            echo "<tr>
+                                <td>".$row['name']."</td>
+                                <td>".$row['surname']."</td>
+                                <td>".$row['email']."</td>
+                                <td>".$row['last_login']."</td>
+                                <td>".$row['ip']."</td>
+                                <td>".$row['access']."</td>
+                                <td><button class='button'>Zmień dane</button></td>
+                            </tr>";
+                        }
+                    }
+                    echo "</table>";
+                }
+            }
             $connect->close();
         ?>        
     <br/><br/></div>
--- scripts/php/new_user_csv.php
+++ scripts/php/new_user_csv.php
@@ -1,17 +1,20 @@
 <?php
     include_once("db_connect.php");
 
-    $file = $_POST('file');
-    $row = 1;
-    $separator = $_POST('separator');
-    $csv = fopen($file,"r");
+    //if(isset($_POST('file'))) {
+        $file = $_POST('file');
+        $row = 1;
+        $separator = $_POST('separator');
+        $csv = fopen($file,"r");
 
-    while(($data = fgetcsv($csv,1000,$separator)) !==FALSE) {
-        $num = count($data);
-        $row++;
-        
-        for($c=0; $c < $num; $c++) {
-            echo $data[$c]."<br/>\n";
+        while(($data = str_getcsv($csv,$separator,'"')) !==FALSE) {
+            $num = count($data);
+            $row++;
+            
+            for($c=0; $c < $num; $c++) {
+                echo $data[$c]."<br/>\n";
+            }
         }
-    }
+        fclose($csv);
+    //}
 ?>
